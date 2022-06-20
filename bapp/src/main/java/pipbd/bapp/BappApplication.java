6fe8752323f30f933cd5928798ea6dde19f38ff9
rafal-ftw <rafal.w.draws@gmail.com>
2022-06-20 15:02:16 +0200

    init

---
+++ bapp/src/main/java/pipbd/bapp/BappApplication.java
@@ -0,0 +1,13 @@
+package pipbd.bapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BappApplication.class, args);
+	}
+
+}
---
+++ bapp/src/test/java/pipbd/bapp/BappApplicationTests.java
@@ -0,0 +1,13 @@
+package pipbd.bapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
